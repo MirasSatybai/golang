414f99cd2de7a16657342a456e71fc15b696bb53
Azatkali <azatkali.16@gmail.com>
2021-04-19 16:21:28 +0600

    feat: created media app and model

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    description = models.TextField()
+    user = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    medias = models.ManyToManyField('medias.Media')
+    post_date = models.DateTimeField(auto_now_add=True)
--- users/models.py
+++ users/models.py
@@ -7,4 +7,5 @@ class User(models.Model):
     phone_number = models.CharField(max_length=12)
     password = models.CharField(max_length=64)
     email = models.EmailField(max_length=64)
-    avatar = models.TextField(default="default_avatar.png")
+    avatar = models.TextField(default='default_avatar.png')
+    friend_list = models.ManyToManyField('self', symmetrical=False)
